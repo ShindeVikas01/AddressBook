ee5b0f7b0ad44dbe2165880ad7febd9d2816db54
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-05-05 17:03:12 +0530

    [ADDED]:[Vikas] Address book project setup

---
+++ src/com/infinity/addressbook/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.infinity.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Welcome message for user
+		 */
+		System.out.println("Welcome to AddressBook Program");
+	}
+
+}
