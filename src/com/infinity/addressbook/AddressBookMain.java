fd4fdb8dd9b0813756be15dc49e6e7a66f864ad4
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-05-05 17:36:01 +0530

    [MODIFIED]:[Vikas] Update contact using First Name in Address book

--- src/com/infinity/addressbook/AddressBookMain.java
+++ src/com/infinity/addressbook/AddressBookMain.java
@@ -1,5 +1,7 @@
 package com.infinity.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
@@ -8,9 +10,35 @@ public class AddressBookMain {
 		 * Welcome message for user
 		 */
 		System.out.println("Welcome to AddressBook Program");
+		
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		
 		AddressBook addressBook = new AddressBook();
-		addressBook.addContact();
-		addressBook.showContact();
+		
+		do
+		{
+			System.out.println("***ADDRESSBOOK MENU***");
+			System.out.println("1. ADD CONTACT \n2. SHOW CONTACT \n3. UPDATE CONTACT \n4. DELETE CONTACT");
+			System.out.println("Enter the CHOICE:");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.showContact();
+				break;
+			case 3:
+				addressBook.updateContact();
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("Invalid Choice");
+					break;
+			}
+		}while(choice<5);
 	}
 
 }
